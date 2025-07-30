95372c598002f549537dc5a4c08681651a0d9e95
SociopathEliot <dlaonlcahik@gmail.com>
2025-07-30 21:38:04 +0600

    Rename welcome and home fragments

---
+++ app/src/main/java/be/buithg/etghaifgte/presentation/ui/fragments/onboarding/BabuIntroFragment.kt
@@ -0,0 +1,35 @@
+package be.buithg.etghaifgte.presentation.ui.fragments.onboarding
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.edit
+import androidx.fragment.app.Fragment
+import be.buithg.etghaifgte.databinding.FragmentBabuIntroBinding
+import be.buithg.etghaifgte.presentation.ui.fragments.main.BabuHomeFragment
+import be.buithg.etghaifgte.utils.BabuAppConstants.BABU_WELCOME_KEY
+import be.buithg.etghaifgte.utils.BabuAppConstants.getBabuPreferences
+import be.buithg.etghaifgte.utils.BabuAppConstants.openBabuFragmentNoHistory
+
+class BabuIntroFragment : Fragment() {
+
+    private lateinit var babuIntroBinding: FragmentBabuIntroBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        babuIntroBinding = FragmentBabuIntroBinding.inflate(inflater, container, false)
+        return babuIntroBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        babuIntroBinding.babuIntroStartButton.setOnClickListener {
+            context?.getBabuPreferences()?.edit { putBoolean(BABU_WELCOME_KEY, true).apply() }
+            parentFragmentManager.openBabuFragmentNoHistory(BabuHomeFragment())
+        }
+    }
+}
